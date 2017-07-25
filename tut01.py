c2f33620a167faf70c57c6fe849877d57c8c8ac1
Jorge A. Garcia <jorgeagr97@gmail.com>
2017-07-25 02:40:57 -0600

    Tutorial 1 - Reading data

--- tut01.py
+++ tut01.py
@@ -1,4 +1,4 @@
-#01-Reading In The Data
+#01-Reading in the data (general properties of the datacubes)
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -6,47 +6,58 @@ from astropy.io import fits
 from astropy.visualization import astropy_mpl_style
 plt.style.use(astropy_mpl_style)
 import matplotlib.image as mpimg
-#from plotfits import plotfits???
 
-#Functions
-def plotfits(subsize,title,imgdata,color,cbarlabel):
-    plt.subplot(subsize)
-    plt.title(title)
-    plt.imshow(imgdata, cmap=color)
-    ibar= plt.colorbar()
-    ibar.set_label(cbarlabel)
-    plt.gca().invert_yaxis()
+#Create Fits Image Object
+class FitsImage(object):
+    
+    #Initializes object, converts fits data into Python array. Detects shape for corresponding plot.
+    def __init__(self,file,title,colorscale,colorlabel):
+        self.file = file
+        self.title = title
+        self.color = colorscale
+        self.colorlabel = colorlabel
+        
+        self.imgdata = fits.getdata(self.file)
+        
+        self.shape = np.shape(self.imgdata)
+        self.dim = len(self.shape)
+    
+    #Plotting method: Plots data and determines location and size of subplot. Grid option boolean.
+    def plot(self,rowdim,coldim,row,col,rowspan,colspan):
+        if self.dim == 3:
+            plt.subplot2grid((rowdim, coldim), (row, col), rowspan = rowspan, colspan = colspan)
+            plt.title(self.title)
+            plt.imshow(self.imgdata[0], cmap = self.color)
+            ibar = plt.colorbar()
+            ibar.set_label(self.colorlabel)
+            plt.gca().invert_yaxis()
+            plt.xticks([])
+            plt.yticks([])
+            plt.grid(False)
+        else:
+            plt.subplot2grid((rowdim, coldim), (row, col), rowspan = rowspan, colspan = colspan)
+            plt.title(self.title)
+            plt.imshow(self.imgdata, cmap = self.color)
+            ibar = plt.colorbar()
+            ibar.set_label(self.colorlabel)
+            plt.gca().invert_yaxis()
+            plt.xticks([])
+            plt.yticks([])
+            plt.grid(False)
 
-#Data Info
-fitsnplot=np.array([['fd_Ic_6h_01d.fits','Intensity','gray','Continuum Intensity'],['fd_M_96m_01.fits','Magnetogram','gray','Gauss'],['fd_V_01h.fits','Dopplergram','RdBu_r','Velocity (m/s)']])
-
-#Data and Text
-fitsimage1=fits.open(fitsnplot[0,0])
-imgdata1=fitsimage1[0].data
-fitsimage2=fits.open(fitsnplot[1,0])
-imgdata2=fitsimage2[0].data
-fitsimage3=fits.open(fitsnplot[2,0])
-imgdata3=fitsimage3[0].data
-
-tuttext1='''Here we have two datacubes, as well as read in and display \nsample images from the MDI instrument taken at during the \nsame interval (2002 March 31 15:00 - 23:32 UT). \nThe data cubes wll be the focus of the remainder of the exrcises. \nHowever, for now, examine the sample Dopplergram, Magnetogram \nand Continuum-Intensity image from the MDI instrument. \nIt is probable that you have seen simlar looking images as the \nintensity image, and magnetogram. The Dopplergram shows \nthe line-of-sight velocity of the photosphere at each pixel. \nA positive velocity indicates motion away from the observer (redshift). \nNotice that the other side of the Dopplergram is blue. Also notice \nthe patterns of red and blue structures which increase in contrast \ntowards the limb. This is supergranulation.'''
-tuttext1q1='''If the units of the Dopplegram are in m/s, and the Sun has a radius of 696 Mm, can you determine th solar rotaion period?'''
-tuttext1q2='''Why do you think supergranulation is more visible near the limb in these Dopplergrams?'''
-tuttext1q3='''Can you see evidence of supergranulation in the continuum image or magnetogram? Try adjusting the minimum and maximum values of the range keyword in the call to tvim to increase the contrast'''
-
-#Figure Format
+#Matplotlib Figure
 plt.figure(1, facecolor = 'white', edgecolor = 'k')
 plt.gcf().canvas.set_window_title('1: Reading in the data')
 
-#Plotting
-plotfits(221,'Intensity',imgdata1,'gray','Continuum Intensity')
-plotfits(222,'Magnetogram',imgdata2,'gray','Gauss')
-plotfits(223,'Dopplergram',imgdata3,'RdBu_r','Velocity (m/s)')
+#Define images
+Intensity = FitsImage('fd_Ic_6h_01d.fits','Intensity','gray','Continuum Intensity')
+Magnetogram = FitsImage('fd_M_96m_01.fits','Magnetogram','gray','Guass')
+Dopplergram = FitsImage('fd_V_01h.fits','Dopplergram','RdBu_r','Velocity (m/s)')
 
-#Text in figure
-plt.figtext(0.75, 0.25,tuttext1, wrap=True, ha='center', fontsize=12, bbox=dict(facecolor='white',edgecolor='black'))
-plt.figtext(0.75, 0.20,tuttext1q1, wrap=True, ha='center', fontsize=12, bbox=dict(facecolor='white',edgecolor='black'))
-plt.figtext(0.75, 0.17,tuttext1q2, wrap=True, ha='center', fontsize=12, bbox=dict(facecolor='white',edgecolor='black'))
-plt.figtext(0.75, 0.102,tuttext1q3, wrap=True, ha='center', fontsize=12, bbox=dict(facecolor='white',edgecolor='black'))
+#Plot images
+Intensity.plot(2,2,0,0,1,1)
+Magnetogram.plot(2,2,0,1,1,1)
+Dopplergram.plot(2,2,1,0,1,1)
 
 #Open Figure Maximized
 mng = plt.get_current_fig_manager()
