8ae00f8e187d346dd54d44b75a2a156f5eb770ce
Jorge A, Garcia <jorgeagr97@gmail.com>
2018-03-17 07:08:50 -0600

    Updates

--- HMI_20110106.py
+++
@@ -1,9 +0,0 @@
-import astropy.units as u
-from sunpy.net import Fido, attrs as a
-from sunpy.database import Database
-
-db = Database()
-db.fetch(a.Time("2011-01-06T00:00:00", "2011-01-06T00:01:00"),
-         a.Instrument('HMI'), a.vso.Sample(1*u.min))
-db.comit()
-db
